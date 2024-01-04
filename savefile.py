418c204a57a341de4ede0bba1b2abd23822e8f42
tienna <tienna@gmail.com>
2024-01-04 08:57:20 +0100

    add init files

---
+++ savefile.py
@@ -0,0 +1,4 @@
+
+with open("base_address.addr", "a", encoding="utf-8") as f:
+    f.write("addr_test1qp0nau7fpeerey2548mgqn504hz868p26ul502trvlhxvfgmch6k75ta5pd9td226kq83qkgepjhwkuv4l7aj4v2jcqqa97aq5")
+
